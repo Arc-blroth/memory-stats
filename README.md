bc200bb8e0abe02855f904cddecb52b984ed30b3
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2022-03-20 12:32:31 -0700

    add docs and doctests

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
 # Memory Stats
 
-A cross-platform-ish memory profiler for Rust.  This crate provides two metrics:
+A cross-platform memory profiler for Rust, supporting Windows, Linux, and MacOS. This crate provides two metrics:
 
 - **"Physical" Memory**, which corresponds to the _Resident Set Size_ on Linux and MacOS and the _Working Set_ on Windows.
 - **"Virtual" Memory**, which corresponds to the _Virtual Size_ on Linux and MacOS and the _Pagefile Usage_ on Windows.
+
+## Usage
+
+Add `memory-stats` as a dependency to your `Cargo.toml`:
+
+```toml
+[dependencies]
+memory-stats = "1.0.0"
+```
+
+## Example
+
+Here's an example that prints out the current memory usage:
+
+```rs
+use memory_stats::memory_stats;
+
+fn main() {
+    if let Some(usage) = memory_stats() {
+        println!("Current physical memory usage: {}", usage.physical_mem);
+        println!("Current virtual memory usage: {}", usage.virtual_mem);
+    } else {
+        println!("Couldn't get the current memory usage :(");
+    }
+}
+```
+
+## Caveats
+
+Getting accurate memory usage on Linux is fairly expensive and not always possible. This crate always attempts to use the statistics from
+[`/proc/self/smaps`](https://man7.org/linux/man-pages/man5/proc.5.html#:~:text=See%20user_namespaces%287%29.-,/proc/%5Bpid%5D/smaps,-%28since%20Linux%202.6.14)
+if avaliable. However, since support for `/proc/self/smaps` might not be compiled in on all kernels, this crate will also use the faster but less accurate statistics from
+[`/proc/self/statm`](https://man7.org/linux/man-pages/man5/proc.5.html#:~:text=by%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20waitpid%282%29.-,/proc/%5Bpid%5D/statm,-Provides%20information%20about)
+as a fallback.
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,41 @@
-#![doc = include_str!("../README.md")]
+//! # Memory Stats
+//!
+//! A cross-platform memory profiler for Rust, supporting Windows, Linux, and MacOS. This crate provides two metrics:
+//!
+//! - **"Physical" Memory**, which corresponds to the _Resident Set Size_ on Linux and MacOS and the _Working Set_ on Windows.
+//! - **"Virtual" Memory**, which corresponds to the _Virtual Size_ on Linux and MacOS and the _Pagefile Usage_ on Windows.
+//!
+//! ## Usage
+//!
+//! Add `memory-stats` as a dependency to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! memory-stats = "1.0.0"
+//! ```
+//!
+//! ## Example
+//!
+//! Here's an example that prints out the current memory usage:
+//!
+//! ```
+//! use memory_stats::memory_stats;
+//!
+//! if let Some(usage) = memory_stats() {
+//!     println!("Current physical memory usage: {}", usage.physical_mem);
+//!     println!("Current virtual memory usage: {}", usage.virtual_mem);
+//! } else {
+//!     println!("Couldn't get the current memory usage :(");
+//! }
+//! ```
+//!
+//! ## Caveats
+//!
+//! Getting accurate memory usage on Linux is fairly expensive and not always possible. This crate always attempts to use the statistics from
+//! [`/proc/self/smaps`](https://man7.org/linux/man-pages/man5/proc.5.html#:~:text=See%20user_namespaces%287%29.-,/proc/%5Bpid%5D/smaps,-%28since%20Linux%202.6.14)
+//! if avaliable. However, since support for `/proc/self/smaps` might not be compiled in on all kernels, this crate will also use the faster but less accurate statistics from
+//! [`/proc/self/statm`](https://man7.org/linux/man-pages/man5/proc.5.html#:~:text=by%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20waitpid%282%29.-,/proc/%5Bpid%5D/statm,-Provides%20information%20about)
+//! as a fallback.
 
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
 #[cfg_attr(any(target_os = "linux", target_os = "android"), path = "linux.rs")]
@@ -37,8 +74,13 @@ pub struct MemoryStats {
 }
 
 /// Returns a snapshot of the the memory used by the
-/// current process. If the current memory usage
-/// cannot be queried, `None` is returned.
+/// current process.
+///
+/// # Errors
+///
+/// If the current memory usage cannot be queried
+/// or `memory_stats` is run on a unsupported platform,
+/// `None` is returned.
 #[cfg_attr(
     not(any(
         target_os = "windows",
